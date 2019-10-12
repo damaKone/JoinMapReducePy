67e06da993241cabb780a72e611da217cb19bacb
DamaMaimouna <44228979+DamaMaimouna@users.noreply.github.com>
2019-10-12 15:00:52 +0200

    Update REDUCER.py

--- REDUCER.py
+++ REDUCER.py
@@ -1,71 +1,48 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 10 21:40:59 2019
 
-@author: PERSO
-"""
+
 
 #!/usr/bin/env python
-  
 import sys
-  
-# maps words to their counts
-foundKey = ""
-foundValue = ""
-isFirst = 1
-currentCount = 0
-currentCountry2digit = "-1"
-currentCountryName = "-1"
-isCountryMappingLine = False
- 
-# input comes from STDIN
+import string
+last_autres = "n"
+last_tel = 1
+tail = 1
 for line in sys.stdin:
-    # remove leading and trailing whitespace
     line = line.strip()
-     
-    try:
-        # parse the input we got from mapper.py
-        tel,nom,prenom,dept,ville,dept2 = line.split('^')
-        """ 
-        #the first line should be a mapping line, otherwise we need to set the currentCountryName to not known
-        if personName == "-1": #this is a new country which may or may not have people in it
-            currentCountryName = countryName
-            currentCountry2digit = country2digit
-            isCountryMappingLine = True
-        else:
-            isCountryMappingLine = False # this is a person we want to count
-         
-        if not isCountryMappingLine: #we only want to count people but use the country line to get the right name
- 
-            #first check to see if the 2digit country info matches up, might be unkown country
-            if currentCountry2digit != country2digit:
-                currentCountry2digit = country2digit
-                currentCountryName = '%s - Unkown Country' % currentCountry2digit
-             
-            currentKey = '%s\t%s' % (currentCountryName,personType)
-             
-            if foundKey != currentKey: #new combo of keys to count
-                if isFirst == 0:
-                    print '%s\t%s' % (foundKey,currentCount)
-                    currentCount = 0 #reset the count
-                else:
-                    isFirst = 0
-             
-                foundKey = currentKey #make the found key what we see so when we loop again can see if we increment or print out
-             
-            currentCount += 1 # we increment anything not in the map list
-    except:
-        pass
- 
-try:
-    print '%s\t%s' % (foundKey,currentCount)
-except:
-    pass
-
-
-
-
-
+    splits = line.split("^")
+    cur_tel = splits[0]
+    autres = splits[1]
+    splitss = autres.split(" ")
+    s = len(splitss)     
+    if cur_tel == last_tel :
+       if s == 4 and tail == 2 :
+        print'%s\t%s\t%s' %(cur_tel,autres,last_autres)
+        x = cur_tel
+        cur_tel = last_tel
+        last_tel = x
+        y = s
+        s = tail
+        tail = y
+        last_autres = autres
+      elif s == 2 and tail == 4 :
+       print'%s\t%s\t%s' %(cur_tel,last_autres,autres)
+       x = cur_tel
+       cur_tel = last_tel
+       last_tel = x
+       y = s
+       s = tail
+       tail = y
+       last_autres = autres
+    else:
+        x = cur_tel
+        cur_tel = last_tel
+        last_tel = x
+        y = s
+        s = tail
+        tail = y
+        last_autres = autres
+        continue
+  
 
 
 ############cat customers.dat countries.dat|./smplMapper.py|sort|./smplReducer.py
